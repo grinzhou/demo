9d3c0d71d25da533fdff45ec828690a6e4125167
Grin Zhou <grin.zhou@gtomato.com>
2020-08-30 12:05:15 +0800

    add: new UI development

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,3 +11,55 @@ export const modal = {
     $('#overlay').hide()
   }
 }
+
+export function debounce(func, wait, immediate) {
+  let timeout
+  return function(...args) {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      timeout = null
+      if (!immediate) func.apply(this, args)
+    }, wait)
+    if (immediate && !timeout) func.apply(this, [...args])
+  }
+}
+
+/**
+ *
+ * @param fn {Function}   实际要执行的函数
+ * @param delay {Number}  执行间隔，单位是毫秒（ms）
+ *
+ * @return {Function}     返回一个“节流”函数
+ */
+
+export function throttle(fn, threshhold = 250, ...args) {
+  // 记录上次执行的时间
+  let last
+
+  // 定时器
+  let timer
+
+  // 返回的函数，每过 threshhold 毫秒就执行一次 fn 函数
+  return function() {
+    // 保存函数调用时的上下文和参数，传递给 fn
+    const context = this
+    const now = +new Date()
+
+    // 如果距离上次执行 fn 函数的时间小于 threshhold，那么就放弃
+    // 执行 fn，并重新计时
+    if (last && now < last + threshhold) {
+      clearTimeout(timer)
+
+      // 保证在当前时间区间结束后，再执行一次 fn
+      timer = setTimeout(() => {
+        last = now
+        fn.apply(context, args)
+      }, threshhold)
+
+      // 在时间区间的最开始和到达指定间隔的时候执行一次 fn
+    } else {
+      last = now
+      fn.apply(context, args)
+    }
+  }
+}
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -4,7 +4,7 @@
 // 引入html模板, 会被作为字符串引入
 import template from './index.html'
 import $ from 'expose-loader?exposes[]=$&exposes[]=jQuery!jquery'
-import { modal } from '../../utils'
+import { modal, throttle } from '../../utils'
 
 // 引入css, 会生成<style>块插入到<head>头中
 import '../../assets/css/rest.css'
@@ -16,12 +16,140 @@ export default class {
   mount(container) {
     document.title = 'Homepage'
     container.innerHTML = template
+
     $('#homeBtnPlay').on('click', () => {
       modal.show()
     })
-    // container.querySelector('.nav-btn').addEventListener('click', evt => {
-    //   console.log(evt.target.getAttribute('data-index'))
-    //   // goPage()
-    // })
+
+
+
+    $(window, document).on('scroll', throttle(onWindowScroll, 600))
   }
+}
+
+const setActivePage = (activePageIndex = 1) => {
+  // const docHeight = pageHeight * $('.page-section').length
+  const pageHeight = $('.page-section').innerHeight()
+
+  $('html,body').animate(
+    { scrollTop: pageHeight * (activePageIndex - 1) },
+    600,
+    () => {
+      $('.page-section').removeClass('active')
+      $('.page-section')
+        .eq(activePageIndex - 1)
+        .addClass('active')
+    }
+  )
+
+  switch (activePageIndex) {
+    case 1:
+      $('.page1 .stop').removeClass('stop')
+      break
+
+    case 2:
+      $('.page1 .illustration').children().addClass('stop')
+      $('.page1 .content-box').addClass('stop')
+      break
+
+    case 3:
+      $('.page1 .stop').removeClass('stop')
+      break
+  }
+}
+
+let beforeScrollTop = $(window).scrollTop() || 0
+const onWindowScroll = () => {
+  const windowScollTop = $(window).scrollTop() || 0
+  const pageHeight = $('.page-section').innerHeight()
+  const delta =
+    beforeScrollTop === windowScollTop
+      ? false
+      : beforeScrollTop > windowScollTop ? 'up' : 'down'
+  // const step = windowScollTop - beforeScrollTop
+
+  if (windowScollTop === pageHeight) {
+    // $('.page1 .content-box').css({
+    //   left: '10%',
+    //   opacity: 1
+    // })
+
+    // $('.page1 .girl').css({
+    //   left: '30%',
+    //   opacity: 1
+    // })
+
+    $('.page1 .illustration').children().addClass('stop')
+    $('.page1 .content-box').addClass('stop')
+  }
+
+  // const item_1_offset = Math.abs($('.page1 .content-box').offset().left)
+  // const item_2_offset = Math.abs($('.page1 .girl').offset().left)
+
+  if (delta === 'down') {
+    // if (windowScollTop > pageHeight * 0.3 && windowScollTop < pageHeight) {
+    //   $('.page1 .content-box').css(
+    //     {
+    //       left: item_1_offset < 200 ? item_1_offset + step * 0.5 : '10%',
+    //       opacity: windowScollTop * 0.05
+    //     },
+    //     100
+    //   )
+
+    //   $('.page1 .girl').css(
+    //     {
+    //       left: item_2_offset > 500 ? item_2_offset - step * 0.5 : '30%',
+    //       opacity: windowScollTop * 0.05
+    //     },
+    //     100
+    //   )
+    // }
+
+    if (
+      windowScollTop > pageHeight * 0.5 &&
+      windowScollTop < pageHeight * 1.1 &&
+      windowScollTop !== pageHeight &&
+      !$('.page-section.active').hasClass('page1')
+    ) {
+      setActivePage(2)
+    } else if (
+      windowScollTop > pageHeight * 1.3 &&
+      !$('.page-section.active').hasClass('page2')
+    ) {
+      setActivePage(3)
+    }
+  } else if (delta === 'up') {
+    // if (
+    //   windowScollTop < pageHeight * 0.5 &&
+    //   windowScollTop > pageHeight * 1.25
+    // ) {
+    //   $('.page1 .content-box').css(
+    //     {
+    //       left: 0,
+    //       opacity: 0
+    //     },
+    //     100
+    //   )
+
+    //   $('.page1 .girl').css(
+    //     {
+    //       left: '50%',
+    //       opacity: 0
+    //     },
+    //     100
+    //   )
+    // }
+
+    if (windowScollTop < pageHeight * 0.75) {
+      setActivePage(1)
+    } else if (
+      windowScollTop < pageHeight * 1.6 &&
+      windowScollTop > pageHeight * 0.9 &&
+      windowScollTop !== pageHeight
+    ) {
+      setActivePage(2)
+    }
+  }
+
+  beforeScrollTop = $(window).scrollTop()
 }
